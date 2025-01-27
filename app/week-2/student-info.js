59deac2b180fd3e1c85018acafd83c2d4be0b5ab
MPinera <marklee.salazar@yahoo.com>
2025-01-27 15:15:34 -0700

    week-2 added

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <div>
+        <h1>
+            Marklee Pinera
+        </h1>
+        <p>
+            <Link>href=https://github.com/MPinera/cprg306-assignments</Link>
+        </p>
+      </div>
+    );
+}
